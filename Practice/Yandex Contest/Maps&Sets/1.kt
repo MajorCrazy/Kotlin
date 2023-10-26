a1e1589381160860419675c1282546821d4bd6da
Nikita <113455964+MajorCrazy@users.noreply.github.com>
2023-10-26 14:13:16 +0300

    Update 1.kt

--- Practice/Yandex Contest/Maps&Sets/1.kt
+++ Practice/Yandex Contest/Maps&Sets/1.kt
@@ -1,9 +1,7 @@
 fun main(args: Array<String>) {
     val goods = readln().replace(",", "").split(" ").chunked(2)
-    println(goods)
     val shop = readln().split(" ")
     val map = goods.map {it[0] to it[1].toInt()}
-    println(map)
     val result = map.filter { (k, v) -> k in shop}.map {it.second }.sum()
     print(result)
 }
